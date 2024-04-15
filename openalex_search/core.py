321d2bb8241e91fd0a2cb08928ce5c29ac03baaa
dionxia <xiayudiandian@gmail.com>
2024-04-14 22:57:19 -0400

    fixed tests

--- openalex_search/core.py
+++ openalex_search/core.py
@@ -24,3 +24,8 @@ def rank_publications_by_citation(data):
         publications, key=lambda x: x.get("cited_by_count", 0), reverse=True
     )
     return sorted_publications
+
+if __name__ == '__main__':
+    data = fetch_abstracts('machine learning')
+    print(data['results'])
+    rank_publications_by_citation(data)
--- tests/test_core.py
+++ tests/test_core.py
@@ -7,9 +7,11 @@ from openalex_search.core import fetch_abstracts, rank_publications_by_citation
 
 def test_fetch_abstracts():
     results = fetch_abstracts("machine learning")
-    assert isinstance(results, list)  # ensure results is a list
+    assert isinstance(results, dict)  # ensure results is a list
 
 def test_rank_publications_by_citation():
-    publications = [{'title': 'Test 1', 'cited_by_count': 10}, {'title': 'Test 2', 'cited_by_count': 20}]
+    publications = {'results':
+                    [{'title': 'Test 1', 'cited_by_count': 10}, 
+                     {'title': 'Test 2', 'cited_by_count': 20}]}
     ranked = rank_publications_by_citation(publications)
     assert ranked[0]['title'] == 'Test 2'
